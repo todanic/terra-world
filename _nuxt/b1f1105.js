f2d114996640ce64a4be134274f7ab6f79137362
Teodora <teodoraodanic96@gmail.com>
2021-03-31 17:38:55 +0200

    Updates

---
+++ _nuxt/b1f1105.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{249:function(n,e,o){"use strict";o.r(e);var t=o(140),c={fetch:function(n){(0,n.redirect)("/"+t.b[0])}},l=o(21),component=Object(l.a)(c,void 0,void 0,!1,null,null,null);e.default=component.exports}}]);
